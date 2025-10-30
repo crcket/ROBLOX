a9161676db949dbe6416b7e451d3aa7e0b3fbd71
846c/crxcket <84820661+crcket@users.noreply.github.com>
2025-10-30 13:07:48 -0400

    Create GithubSHAChecker.lua

---
+++ GithubSHAChecker.lua
@@ -0,0 +1,25 @@
+local Checker = {}
+
+function Checker.CheckGithubSHA(githubUrl)
+    local user, repo, ref = githubUrl:match('githubusercontent%.com/([^/]+)/([^/]+)/(.+)')
+    if not user or not repo or not ref then return end
+    local branch = ref:match('refs/heads/([^/]+)')
+    local remainingPath = ref:match('refs/heads/[^/]+/(.+)')
+    if not branch then
+        branch = ref:match('^([a-f0-9]+)')
+        remainingPath = ref:match('^[a-f0-9]+/(.+)')
+    end
+    branch = branch or 'main'
+    local apiUrl = string.format('https://api.github.com/repos/%s/%s/commits/%s', user, repo, branch)
+    local response = request({
+        Url = apiUrl,
+        Method = 'GET',
+        Headers = { ['User-Agent'] = 'GTC' },
+    })
+    if not response.Success then return end
+    local data = game:GetService('HttpService'):JSONDecode(response.Body)
+    if not data or not data.sha then return end
+    return data.sha:sub(1, 7)
+end
+
+return Checker
